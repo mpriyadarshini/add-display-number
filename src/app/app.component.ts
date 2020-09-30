3b9197890f722b5543de7eb1e3ba2229d3f92e80
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2020-09-30 22:39:27 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  array = [];
+
+  valueTextField = new FormControl('', [Validators.required, Validators.pattern("^[0-9]+$")]);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  submit() {
+    this.array.push(parseInt(this.valueTextField.value));
+    console.log(this.array);
+    this.valueTextField.reset();
+  }
+
+  getColor(num:number) {
+    return num % 3 === 0 ? 'green':'white';
+  }
+
+  
+  
 }
+
+
